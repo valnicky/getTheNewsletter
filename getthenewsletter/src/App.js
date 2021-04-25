43b0d41f3b2bb74d725e33e8d5353f6bed086cf1
Valeria <valery0007@gmail.com>
2021-04-25 20:43:58 +0200

    changes

--- getthenewsletter/src/App.js
+++ getthenewsletter/src/App.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import Newsletter from './components/Newsletter.js';
 import './App.css';
-import {ThemeProvider} from './ThemeContext.js';
+import './NewsletterOverride.css';
 
 const theme = {
-  header: {
-    fg: '#ff598a',
-  },
-  input: {
-    color: '#fff',
-    background: '#070222',
-    textAlign: 'center'
-  },
-  inputFocus: {
-    outline: '2px solid #5e9fff'
-  }
+    header: {
+        fg: '#ff598a',
+    },
+    input: {
+        color: '#fff',
+        background: '#070222',
+        textAlign: 'center'
+    },
+    inputFocus: {
+        outline: '2px solid #5e9fff'
+    }
 }
 
 function App() {
-  return (
-  
-    <div className="app__newsletter">
-        <ThemeProvider>
-          <Newsletter/>
+    return (
+        <div className = "app__newsletter" >
+        <ThemeProvider theme = { theme } >
+        <Newsletter />
         </ThemeProvider>
-      
-</div>
-  );
+        </div>
+    );
 }
 
 export default App;
--- getthenewsletter/src/components/Newsletter.js
+++ getthenewsletter/src/components/Newsletter.js
@@ -1,95 +1,49 @@
-import React from 'react'
-
-import {ThemeContext} from '../ThemeContext.js';
+import React from 'react';
+import  '../Newsletter.css';
 
 function Newsletter(props) {
     const [email, setEmail] = React.useState('');
     const [emailFocused, setEmailFocused] = React.useState(false);
-    const {width} = useWindowDimensions();
     
-    return ( < section style={styles.container({width})}>
-        <div style={styles.spectrum()} arria-hidden>
+  
+    const emailPartsCount = countEmailParts(email);
+    
+    return ( < section className="es-newsletter">
+        <div className="es-newsletter__spectrum"  arria-hidden>
             {Array.from(Array(5)).map((_, i)=>(
-                <div key={i} ></div>
-            )
-                )}
-            
+                <div className={`es-newsletter__bar ${i + 1 <= emailPartsCount && 'es-newsletter__bar--active'}`}
+                key={i} ></div>
+            ))}
         </div>
         
-        <header >
-        <h2  > Get the newsletter </h2> </header> 
-        < input type = "email"
+        <header className="es-newsletter__header">
+        <h2> Get the newsletter </h2> </header> 
+        < input className="es-newsletter__email"
+        type = "email"
         placeholder = "Your email"
-
+value={email}
+onChange={(evt)=> setEmail(evt.target.value)}
+onFocus = {()=> setEmailFocused(true)}
+onBlur = {()=>setEmailFocused(false)}
      />
-        <button > Sign up </button> </section>
+        <button className={`es-newsletter__submit`}> Sign up </button> </section>
     )
 }
 
 export default Newsletter;
 
-const color={
-    spectrum1: '#ff598a',
-    spectrum2: '#de56e8',
-    spectrum3: '#b36bff',
-    spectrum4: '#5b56e8',
-    spectrum5: '5e9fff'
-}
-
-const styles= {
-    container: ({width})=>({
-        position: 'relative',
-        maxWidth: width >= 800 ? '700px' : '100%',
-        fontSize: width>= 800 ? '2.25em' : '1.25em',
-        padding: '1em 1em 2em 1em',
-        background: '#2b283d'
-    }),
-    spectrum: () =>({
-        position: 'absolute', 
-    top: '0',
-    bottom: '0',
-    left: '0',
-    width: '100%',
-    display: 'flex',
-    alignItems: 'flex-end',
-    pointerEvents: 'none'
-    }),
-    bar: ({i})=>({
-        height: '0.5em',
-        width: '20%',
-        transformOrigin: 'bottom',
-        transition: 'all 1s',
-        background: color[Object.keys(color)[i%Object.keys(color).length> 0]]
-    }),
-    header: ()=>({
-        position: 'relative',
-        color: 'white',
-        zIndex: '1',
-        textTransform: 'uppercase',
-        fontSize: '0.85em',
-        textShadow: '0 3px 2px #000',
-    }),
-    headerH2: ()=>({
-        
-    }),
-    
-    
-    
-    
-    
-}
-
-function useWindowDimensions(){
-    const[windowDimensions, setWindow] = React.useState({
-        width: window.innerWidth
-    })
-    
-    React.useEffect(()=>{
-        function handleResize(){
-            setWindow({width: window.innerWidth})
-        }
-        window.addEventListener('resize', handleResize)
-        return ()=> window.removeEventListener('resize', handleResize)
-    })
-    return windowDimensions
+function countEmailParts(email){
+    if(/@.+\..{2,}$/.test(email)){
+        return 5;
+    } else if(/@.+\..?$/.test(email)){
+        return  4;
+    }else if(/@.+$/.test(email)){
+        return 3;
+    }else if(/@/.test(email)){
+        return 2;
+    }else if(/.+/.test(email)){
+        return 1;
+    }else{
+        return 0;
+    }
 }
